e821e8496a9d6aa6c07af43ea18c9860e26795af
Moskvitin Andrey <archimag@gmail.com>
2012-12-28 22:53:54 +0400

    Refactoring based on recent changes in RESTAS.

--- core/routes.lisp
+++ core/routes.lisp
@@ -2,6 +2,9 @@
 
 (in-package #:arblog)
 
+(defun parse-skip-param ()
+  (ignore-errors (parse-integer (hunchentoot:get-parameter "skip"))))
+
 (defun url-with-skip (url skip)
   (let ((parsed-url (puri:parse-uri url)))
     (setf (puri:uri-query parsed-url)
@@ -18,23 +21,31 @@
                   (url-with-skip url (- skip *posts-on-page*)))
                  (t url))))
 
+(defmethod data-sift:compile-rule ((rule (eql 'year)) &key)
+  (data-sift:compile-rule 'integer))
+
+(defmethod data-sift:compile-rule ((rule (eql 'month)) &key)
+  (data-sift:compile-rule '(integer :min-value 1 :max-value 12)))
+
+(defmethod data-sift:compile-rule ((rule (eql 'day)) &key)
+  (data-sift:compile-rule '(integer :min-value 1 :max-value 31)))
+
 ;;;; main page
 
 (restas:define-route entry ("")
-  (let ((skip (or (ignore-errors (parse-integer (hunchentoot:get-parameter "skip"))) 0)))
-    (render.list-recent-posts
-     (ds.list-recent-posts skip *posts-on-page*)
-     (navigation (restas:genurl 'entry)
-                 skip
-                 (ds.count-posts)))))
+  (:apply-render-method #'render.list-recent-posts)
+  (:additional-variables (skip (parse-skip-param) 0))
+  (list (ds.list-recent-posts skip *posts-on-page*)
+        (navigation (restas:genurl 'entry)
+                    skip
+                    (ds.count-posts))))
 
 ;;;; One post
 
-(restas:define-route one-post (":year/:month/:day/:urlname"
-                               :parse-vars (list :year #'parse-integer
-                                                 :month #'parse-integer
-                                                 :day #'parse-integer))
-  (render.one-post (ds.find-single-post year month day urlname)))
+(restas:define-route one-post (":year/:month/:day/:urlname")
+  (:sift-variables (year 'year) (month 'month) (day 'day))
+  (:render-method #'render.one-post)
+  (ds.find-single-post year month day urlname))
 
 (restas:define-route post-permalink ("permalink/posts/:id")
   (let* ((info (ds.get-single-post id :fields '("published" "urlname")))
@@ -48,8 +59,9 @@
 
 ;;;; archive
 
-(restas:define-route archive-for-year (":year/"
-                                       :parse-vars (list :year #'parse-integer))
+(restas:define-route archive-for-year (":year/" )
+  (:sift-variables (year 'year))
+  (:apply-render-method #'render.archive-for-year)
   (let* ((min (local-time:encode-timestamp 0 0 0 0 1 1 year))
          (max (local-time:adjust-timestamp min (offset :year 1)))
          (posts (ds.list-archive-posts min max '("published")))
@@ -57,38 +69,39 @@
     (dolist (post posts)
       (setf (gethash (local-time:timestamp-month (gethash "published" post)) months)
             1))
-    (render.archive-for-year year
-                             (sort (iter (for (month x) in-hashtable months)
-                                         (collect month))
-                                   #'<))))
+    (list year
+          (sort (iter (for (month x) in-hashtable months)
+                      (collect month))
+                #'<))))
 
-(restas:define-route archive-for-month (":year/:month/"
-                                        :parse-vars (list :year #'parse-integer
-                                                          :month #'parse-integer))
+(restas:define-route archive-for-month (":year/:month/")
+  (:sift-variables (year 'year) (month 'month))
+  (:apply-render-method #'render.archive-for-month)
   (let* ((min (local-time:encode-timestamp 0 0 0 0 1 month year))
          (max (local-time:adjust-timestamp min (offset :month 1))))
-    (render.archive-for-month year month (ds.list-archive-posts min max))))
+    (list year month (ds.list-archive-posts min max))))
 
-(restas:define-route archive-for-day (":year/:month/:day/"
-                                      :parse-vars (list :year #'parse-integer
-                                                        :month #'parse-integer
-                                                        :day #'parse-integer))
+(restas:define-route archive-for-day (":year/:month/:day/")
+  (:sift-variables (year 'year) (month 'month) (day 'day))
+  (:apply-render-method #'render.archive-for-day)
   (let* ((min (local-time:encode-timestamp 0 0 0 0 day month year))
          (max (local-time:adjust-timestamp min (offset :day 1))))
-    (render.archive-for-day year month day (ds.list-archive-posts min max))))
+    (list year month day (ds.list-archive-posts min max))))
 
 ;;;; Tags
 
 (restas:define-route all-tags ("tags/")
-  (render.all-tags (ds.all-tags)))
+  (:render-method #'render.all-tags)
+  (ds.all-tags))
 
 (restas:define-route posts-with-tag ("tags/:tag")
-  (let ((skip (or (ignore-errors (parse-integer (hunchentoot:get-parameter "skip"))) 0)))
-    (render.posts-with-tag tag
-                           (ds.list-recent-posts skip *posts-on-page* :tag tag)
-                           (navigation (restas:genurl 'posts-with-tag :tag tag)
-                                       skip
-                                       (ds.count-posts tag)))))
+  (:apply-render-method #'render.posts-with-tag)
+  (:additional-variables (skip (parse-skip-param) 0))
+  (list tag
+        (ds.list-recent-posts skip *posts-on-page* :tag tag)
+        (navigation (restas:genurl 'posts-with-tag :tag tag)
+                    skip
+                    (ds.count-posts tag))))
 
 ;;;; Feeds
 
@@ -100,30 +113,33 @@
         :updated (local-time:format-timestring nil (gethash "updated" post))
         :content (gethash "content" post)))  
 
-(restas:define-route posts-feed ("feeds/atom"
-                                 :content-type "application/atom+xml")
-  (arblog.feed.tmpl:atom-feed
-   (list :name *blog-name*
-         :href-atom (restas:gen-full-url 'posts-feed)
-         :href-html (restas:gen-full-url 'entry)
-         :posts (mapcar #'feed-post-info (ds.list-recent-posts 0 50)))))
+(restas:define-route posts-feed ("feeds/atom" :content-type "application/atom+xml")
+  (:render-method #'arblog.feed.tmpl:atom-feed)
+  (list :name *blog-name*
+        :href-atom (restas:gen-full-url 'posts-feed)
+        :href-html (restas:gen-full-url 'entry)
+        :posts (mapcar #'feed-post-info (ds.list-recent-posts 0 50))))
 
-(restas:define-route posts-with-tag-feed ("feeds/atom/tag/:tag"
-                                          :content-type "application/atom+xml")
-  (arblog.feed.tmpl:atom-feed
-   (list :name (format nil "~A blog posts with tag \"~A\"" *blog-name* tag)
-         :href-atom (restas:gen-full-url 'posts-feed)
-         :href-html (restas:gen-full-url 'entry)
-         :posts (mapcar #'feed-post-info (ds.list-recent-posts 0 50)))))
+(restas:define-route posts-with-tag-feed ("feeds/atom/tag/:tag" :content-type "application/atom+xml")
+  (:render-method #'arblog.feed.tmpl:atom-feed)
+  (list :name (format nil "~A blog posts with tag \"~A\"" *blog-name* tag)
+        :href-atom (restas:gen-full-url 'posts-feed)
+        :href-html (restas:gen-full-url 'entry)
+        :posts (mapcar #'feed-post-info (ds.list-recent-posts 0 50))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Admin
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun check-admin-rights ()
+(defclass admin-route (routes:proxy-route) ())
+
+(defmethod restas:process-route :before ((route admin-route) bindings)
   (multiple-value-bind (user password) (hunchentoot:authorization)
-    (or (ds.check-admin user password)
-        (hunchentoot:require-authorization))))
+    (unless (ds.check-admin user password)
+      (hunchentoot:require-authorization))))
+
+(defun @admin (route)
+  (make-instance 'admin-route :target route))
 
 (defun form-action-p (method)
   (alexandria:named-lambda required-form-action ()
@@ -142,79 +158,89 @@
 ;; main admin page
 
 (restas:define-route admin-entry ("admin/")
-  (check-admin-rights)
-  (let ((skip (or (ignore-errors (parse-integer (hunchentoot:get-parameter "skip"))) 0))
-        (*posts-on-page* 25))
-    (render.admin-posts (ds.list-recent-posts skip *posts-on-page*
-                                              :fields '("title" "published"))
-                        (navigation (restas:genurl 'admin-entry)
-                                    skip
-                                    (ds.count-posts)))))
+  (:decorators #'@admin)
+  (:apply-render-method 'render.admin-posts)
+  (:additional-variables (skip (parse-skip-param) 0))
+  (let ((*posts-on-page* 25))
+    (list (ds.list-recent-posts skip *posts-on-page*
+                                :fields '("title" "published"))
+          (navigation (restas:genurl 'admin-entry)
+                      skip
+                      (ds.count-posts)))))
 
 ;; create post
 
 (restas:define-route admin-create-post ("admin/create-post")
-  (check-admin-rights)
-  (render.admin-edit-post))
+  (:decorators '@admin)
+  (:apply-render-method #'render.admin-edit-post))
 
-(restas:define-route admin-cancel-create-post ("admin/create-post"
-                                               :method :post
-                                               :requirement (form-action-p "cancel"))
-  (check-admin-rights)
+(restas:define-route admin-cancel-create-post ("admin/create-post" :method :post)
+  (:requirement (form-action-p "cancel"))
+  (:decorators '@admin)
   (restas:redirect 'admin-entry))
 
-(restas:define-route admin-preview-create-post ("admin/create-post"
-                                                :method :post
-                                                :requirement (form-action-p "preview"))
-  (check-admin-rights)
-  (preview-post))
+(restas:define-route admin-preview-create-post ("admin/create-post" :method :post)
+  (:requirement (form-action-p "preview"))
+  (:decorators '@admin)
+  (:apply-render-method #'render.admin-edit-post)
+  (:additional-variables (markup (hunchentoot:post-parameter "content"))
+                         (title (hunchentoot:post-parameter "title"))
+                         (tags (post-parameter-tags)))
+  (list :title title
+        :markup markup
+        :tags tags
+        :preview (markup.render-content markup)))
 
-(restas:define-route admin-save-create-post ("admin/create-post"
-                                             :method :post
-                                             :requirement (form-action-p "save"))
-  (check-admin-rights)
-  (let* ((markup (hunchentoot:post-parameter "content"))
-         (id (ds.insert-post (hunchentoot:post-parameter "title")
-                             (post-parameter-tags)
-                             (markup.render-content markup)
-                             :markup markup)))
-    (break "~A" id)
+(restas:define-route admin-save-create-post ("admin/create-post" :method :post)
+  (:requirement (form-action-p "save"))
+  (:decorators '@admin)
+  (:additional-variables (markup (hunchentoot:post-parameter "content"))
+                         (title (hunchentoot:post-parameter "title"))
+                         (tags (post-parameter-tags)))
+  (let* ((id (ds.insert-post title tags (markup.render-content markup) :markup markup)))
     (restas:redirect 'post-permalink :id id)))
 
 ;; edit post
 
 (restas:define-route admin-edit-post ("admin/:id")
-  (check-admin-rights)
-  (let ((post (ds.get-single-post id)))
-    (render.admin-edit-post :title (gethash "title" post)
-                            :markup (gethash "markup" post)
-                            :tags (gethash "tags" post)
-                            :preview (markup.render-content (gethash "markup" post)))))
+  (:decorators '@admin)
+  (:apply-render-method 'render.admin-edit-post)
+  (let* ((post (ds.get-single-post id)))
+    (list :title (gethash "title" post)
+          :markup (gethash "markup" post)
+          :tags (gethash "tags" post)
+          :preview (gethash "content" post))))
 
-(restas:define-route admin-cancel-edit-post ("admin/:id"
-                                             :method :post
-                                             :requirement (form-action-p "cancel"))
+(restas:define-route admin-cancel-edit-post ("admin/:id" :method :post)
+  (:decorators '@admin)
+  (:requirement (form-action-p "cancel"))
   (declare (ignore id))
-  (check-admin-rights)
   (restas:redirect 'admin-entry))
 
-(restas:define-route admin-preview-edit-post ("admin/:id"
-                                              :method :post
-                                              :requirement (form-action-p "preview"))
+(restas:define-route admin-preview-edit-post ("admin/:id" :method :post)
+  (:decorators '@admin)
+  (:requirement (form-action-p "preview"))
+  (:apply-render-method #'render.admin-edit-post)
+  (:additional-variables (markup (hunchentoot:post-parameter "content"))
+                         (title (hunchentoot:post-parameter "title"))
+                         (tags (post-parameter-tags)))
   (declare (ignore id))
-  (check-admin-rights)
-  (preview-post))
+  (list :title title
+        :markup markup
+        :tags tags
+        :preview (markup.render-content markup)))
 
-(restas:define-route admin-save-edit-post ("admin/:id"
-                                           :method :post
-                                           :requirement (form-action-p "save"))
-  (check-admin-rights)
-  (let ((markup (hunchentoot:post-parameter "content")))
-    (ds.update-post id
-                    (hunchentoot:post-parameter "title")
-                    (post-parameter-tags)
-                    (markup.render-content markup)
-                    :markup markup))
+(restas:define-route admin-save-edit-post ("admin/:id" :method :post)
+  (:decorators '@admin)
+  (:requirement (form-action-p "save"))
+  (:additional-variables (markup (hunchentoot:post-parameter "content"))
+                         (title (hunchentoot:post-parameter "title"))
+                         (tags (post-parameter-tags)))
+  (ds.update-post id
+                  title
+                  tags
+                  (markup.render-content markup)
+                  :markup markup)
   (restas:redirect 'post-permalink :id id))
 
 ;;;; static files
@@ -232,5 +258,3 @@
     (unless (fad:file-exists-p file)
       (restas:abort-route-handler hunchentoot:+http-not-found+))
     file))
-
-
